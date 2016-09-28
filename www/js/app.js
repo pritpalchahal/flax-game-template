9e371329a721a93e6de927d8f5cb65dc3ecaac0a
prit <prit.chahal@gmail.com>
2016-09-29 00:39:06 +1300

    removed Keyboard plugin

--- www/js/app.js
+++ www/js/app.js
@@ -52,17 +52,6 @@ angular.module('collocationmatching', ['ionic', 'collocationmatching.controllers
   var isEdge = ionic.Platform.isEdge();//works for windows 10 phones
 
   $ionicPlatform.ready(function() {
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
@@ -128,8 +117,8 @@ angular.module('collocationmatching', ['ionic', 'collocationmatching.controllers
   //to override default behaviors of specific platforms (android,ios etc)
   //e.g. android align its titles to left by default, so needs to change it here
   //refer to docs http://ionicframework.com/docs/api/provider/$ionicConfigProvider/
+  $ionicConfigProvider.navBar.alignTitle('center');
   if(!ionic.Platform.isEdge()){
-    $ionicConfigProvider.navBar.alignTitle('center');
     //change default back button and text with custom image
     $ionicConfigProvider.backButton.text("");
     $ionicConfigProvider.backButton.icon('my-back-button');
